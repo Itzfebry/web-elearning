f0c3bb4b6a23fa4f4a13c10c41fdc8a825c3cd9f
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-04-18 22:34:40 +0700

    main: fix submit tugas api

--- app/Repositories/ApiSubmitTugasRepository.php
+++ app/Repositories/ApiSubmitTugasRepository.php
@@ -17,25 +17,25 @@ class ApiSubmitTugasRepository
 
     public function store($request)
     {
+        $tanggal = now()->format('Y-m-d');
         $request->validate([
             'tugas_id' => 'required|exists:tugas,id',
             'nisn' => 'required|string|max:12',
             'text' => 'nullable|required_without:file|string',
             'file' => 'nullable|required_without:text|file|mimes:pdf,jpg,png',
-            'tanggal' => 'required|date',
         ]);
 
         $filePath = null;
 
         if ($request->hasFile('file')) {
             $filename = Str::uuid() . '.' . $request->file->extension();
-            $filePath = $request->file->storeAs('uploads/submit_tugas', $filename, 'public');
+            $filePath = $request->file->storeAs('tugas/submit_tugas', $filename, 'public');
         }
 
         $submit = SubmitTugas::create([
             'tugas_id' => $request->tugas_id,
             'nisn' => $request->nisn,
-            'tanggal' => $request->tanggal,
+            'tanggal' => $tanggal,
             'text' => $request->text,
             'file' => $filePath,
         ]);
